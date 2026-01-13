861e22adca078c4fc73c92a175cc70c9e84d8ef7
Ujjwal Gautam <1716540037@kit.ac.in>
2026-01-14 00:00:19 +0530

    week 1 day 2

--- 01-core-java/src/com/ujjwal/java/core/basics/ProgramStructure.java
+++ 01-core-java/src/com/ujjwal/java/core/basics/ProgramStructure.java
@@ -1,4 +1,9 @@
 package com.ujjwal.java.core.basics;
 
 public class ProgramStructure {
+
+    public static void main(String[] args) {
+        System.out.println("Will Learn What is the structure of the java program and the execution takes place in the Java");
+        System.out.println("Understanding Java program structure");
+    }
 }
