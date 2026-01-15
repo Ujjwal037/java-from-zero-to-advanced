2f9078bd46daf0221052862e77c5db73ddeefa39
Ujjwal Gautam <1716540037@kit.ac.in>
2026-01-15 23:31:17 +0530

    Week 1 Day 5: Type Casting and Data Conversion

---
+++ 01-core-java/src/com/ujjwal/java/core/basics/TypeCastingDemo.java
@@ -0,0 +1,29 @@
+package com.ujjwal.java.core.basics;
+
+public class TypeCastingDemo {
+
+    public static void main(String[] args) {
+
+        // Widening casting (automatic)
+        int intValue = 100;
+        double doubleValue = intValue;
+
+        // Narrowing casting (explicit)
+        double price = 99.99;
+        int roundedPrice = (int) price;
+
+        // Data loss example
+        int bigNumber = 130;
+        byte smallNumber = (byte) bigNumber;
+
+        // char to int
+        char letter = 'A';
+        int asciiValue = letter;
+
+        System.out.println("doubleValue: " + doubleValue);
+        System.out.println("roundedPrice: " + roundedPrice);
+        System.out.println("smallNumber: " + smallNumber);
+        System.out.println("asciiValue: " + asciiValue);
+    }
+}
+
