6f50fc0145e6c700ab867be26b57c7c1d39cd375
Ujjwal Gautam <1716540037@kit.ac.in>
2026-01-24 00:10:36 +0530

    Week 2 Day 5: Inheritance

---
+++ 02-OOP/src/com/ujjwal/java/oop/inheritance/InheritanceDemo.java
@@ -0,0 +1,10 @@
+package com.ujjwal.java.oop.inheritance;
+
+public class InheritanceDemo {
+
+    public static void main(String[] args) {
+        Employee emp = new Employee("Ujjwal", 24, "EMP101", 75000);
+        emp.displayInfo();
+    }
+}
+
