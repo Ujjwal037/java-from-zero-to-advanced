a88cd898869a04b8494fe69f1b874f0817038b84
Ujjwal Gautam <1716540037@kit.ac.in>
2026-01-25 01:26:09 +0530

    Week2 Polymorphism

---
+++ 02-OOP/src/com/ujjwal/java/oop/polymorphism/PolymorphismDemo.java
@@ -0,0 +1,19 @@
+package com.ujjwal.java.oop.polymorphism;
+
+public class PolymorphismDemo {
+
+    public static void main(String[] args) {
+
+        // Upcasting (Runtime Polymorphism)
+        Shape shape = new Circle();
+        shape.draw(); // Circle's draw() executes
+
+        // Downcasting
+        if (shape instanceof Circle) {
+            Circle circle = (Circle) shape;
+            circle.radius();
+        }
+    }
+}
+
+
