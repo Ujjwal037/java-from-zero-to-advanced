430282527ebe01eb4f24c0920d25ac6c84807efe
Ujjwal Gautam <1716540037@kit.ac.in>
2026-01-25 01:22:44 +0530

    Week 2 Polymorphism

--- 02-OOP/src/com/ujjwal/java/oop/polymorphism/Circle.java
+++ 02-OOP/src/com/ujjwal/java/oop/polymorphism/Circle.java
@@ -1,4 +1,14 @@
 package com.ujjwal.java.oop.polymorphism;
 
-public class Circle {
+public class Circle extends Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing a circle");
+    }
+
+    public void radius() {
+        System.out.println("Circle radius logic");
+    }
 }
+
