8bbfdcfa7192f6d47a2ff95244f12a8e2bddc431
Ujjwal Gautam <1716540037@kit.ac.in>
2026-01-29 22:48:51 +0530

    Week 3: Custom Exception

---
+++ 03-exceptionHandling/src/com/ujjwal/java/exception/custom/InvalidBalanceException.java
@@ -0,0 +1,9 @@
+package com.ujjwal.java.exception.custom;
+
+public class InvalidBalanceException extends Exception {
+
+    public InvalidBalanceException(String message) {
+        super(message);
+    }
+}
+
