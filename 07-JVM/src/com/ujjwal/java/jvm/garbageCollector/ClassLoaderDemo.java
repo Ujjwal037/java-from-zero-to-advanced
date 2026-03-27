68ddc94117ae8b7f2f7ad69c94658899f37378f6
Ujjwal Gautam <1716540037@kit.ac.in>
2026-03-27 23:06:30 +0530

    Week 7 added classloaderdemo

--- 07-JVM/src/com/ujjwal/java/jvm/garbageCollector/ClassLoaderDemo.java
+++ 07-JVM/src/com/ujjwal/java/jvm/garbageCollector/ClassLoaderDemo.java
@@ -1,4 +1,18 @@
 package com.ujjwal.java.jvm.garbageCollector;
 
 public class ClassLoaderDemo {
+
+    public static void main(String[] args) {
+
+        ClassLoader appLoader =
+                ClassLoaderDemo.class.getClassLoader();
+
+        System.out.println("Application Loader: " + appLoader);
+
+        System.out.println("Parent Loader: " +
+                appLoader.getParent());
+
+        System.out.println("Bootstrap Loader: " +
+                appLoader.getParent().getParent());
+    }
 }
