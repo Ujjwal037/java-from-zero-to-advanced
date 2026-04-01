ef1c50b1576be59e7844a7e70b6a3e45b8d0ca51
Ujjwal Gautam <1716540037@kit.ac.in>
2026-04-01 23:38:06 +0530

    Week 10

---
+++ 10-designpattern/src/com/ujjwal/java/designpattern/Creational/SingletonBestApproach.java
@@ -0,0 +1,17 @@
+package com.ujjwal.java.designpattern.Creational;
+
+public class SingletonBestApproach {
+//    BEST Singleton (Industry Standard)
+    
+    static class Singleton {
+        private Singleton() {}
+
+        private static class Holder {
+            private static final Singleton INSTANCE = new Singleton();
+        }
+
+        public static Singleton getInstance() {
+            return Holder.INSTANCE;
+        }
+    }
+}
