7bbde7e5391300a864f452c23e1810728afd905e
Ujjwal Gautam <1716540037@kit.ac.in>
2026-04-01 23:32:14 +0530

    Week 10

---
+++ 10-designpattern/src/com/ujjwal/java/designpattern/Creational/SingletonWithVolatile.java
@@ -0,0 +1,24 @@
+package com.ujjwal.java.designpattern.Creational;
+
+
+//Best Approach: Double-Checked Locking (IMPORTANT)
+
+public class SingletonWithVolatile {
+
+    static class Singleton {
+        private static volatile Singleton instance;
+
+        private Singleton() {}
+
+        public static Singleton getInstance() {
+            if (instance == null) {
+                synchronized (Singleton.class) {
+                    if (instance == null) {
+                        instance = new Singleton();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+}
